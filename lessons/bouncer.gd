ad988a8875d086d2479e9358e7fffcaa81a3fa58
Yy101488 <yili25@s.sfusd.edu>
2025-12-14 01:24:22 -0800

    Middle of L15

--- lessons/bouncer.gd
+++ lessons/bouncer.gd
@@ -5,11 +5,12 @@ extends CharacterBody2D
 @onready var _dust: GPUParticles2D = %Dust
 @onready var _runner_visual_purple: RunnerVisual = %RunnerVisualPurple
 @onready var _hit_box: Area2D = %HitBox
+@onready var _raycasts: Node2D = %Raycasts
 
 
 @export var max_speed:=600.0
 @export var acceleration:=1200.0
-@export var deceleration := 1080.0
+@export var avoidance_strength :=21000.0
 
 func _physics_process(delta: float) -> void:
 	var direction := global_position.direction_to(get_global_player_position())
@@ -17,11 +18,14 @@ func _physics_process(delta: float) -> void:
 	var speed := max_speed if distance > 100 else max_speed * distance/100
 	
 	var desired_velocity := direction* speed
+	desired_velocity += calculate_avoidance_force() * delta
+
 	velocity= velocity.move_toward(desired_velocity,acceleration*delta)
 	move_and_slide()
 	
-	if velocity.length()>0.0:
+	if velocity.length()>10.0:
 		_runner_visual_purple.angle = rotate_toward(_runner_visual_purple.angle, direction.orthogonal().angle(), 8.0 * delta)
+		_raycasts.rotation = _runner_visual_purple.angle
 		var current_speed_percent:=velocity.length()/max_speed
 		_runner_visual_purple.animation_name=(
 			RunnerVisual.Animations.WALK
@@ -42,3 +46,16 @@ func _ready() -> void:
 		if body is Runner:
 			get_tree().reload_current_scene.call_deferred()
 			)
+func calculate_avoidance_force() -> Vector2:
+	var avoidance_force := Vector2.ZERO
+
+	for raycast: RayCast2D in _raycasts.get_children():
+		if raycast.is_colliding():
+			var collision_position := raycast.get_collision_point()
+			var direction_away_from_obstacle := collision_position.direction_to(raycast.global_position)
+			var ray_length := raycast.target_position.length()
+			var intensity := 1.0 - collision_position.distance_to(raycast.global_position) / ray_length
+			
+			var force := direction_away_from_obstacle * avoidance_strength * intensity
+			avoidance_force += force
+	return avoidance_force
